5ff3234860eca3cc9b85a601e8f8ca6b17d70acf
RomanMereneanu <asasin84@mail.ru>
2019-12-16 13:01:12 +0200

    add content

--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -11,6 +11,31 @@ const Tasks = () => {
                 Price
                 <img src={editSvg} alt="Edit"/>
             </h2>
+
+            <div className="tasks__items">
+                <div className="tasks__items-row">
+                    <div className="checkbox">
+                        <input type="checkbox" id="check"/>
+                        <label htmlFor="check">
+                            <svg
+                                width="11"
+                                height="8"
+                                viewBox="0 0 11 8"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    d="M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001"
+                                    stroke="#000"
+                                    strokeWidth="1.5"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                />
+                            </svg>
+                        </label>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
